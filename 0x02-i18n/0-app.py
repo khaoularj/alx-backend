7adc7acaf45e1432fcc2c1a5ae298ebdba76c695
khaoularj <khaoula.raja1@gmail.com>
2024-05-14 08:01:05 -0700

    adding comments to pass pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+"""setting up a basic flask app"""
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def Hello():
+    """this is the home page"""
     return render_template('0-index.html')
 
 if __name__ == '__main__':
