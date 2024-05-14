74e1375bc24fb0ec4ae8be9872c9b497a44ae4d5
khaoularj <khaoula.raja1@gmail.com>
2024-05-14 09:03:06 -0700

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """setting up a basic flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 from config import Config
 
 
