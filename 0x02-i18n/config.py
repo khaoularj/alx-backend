b1c285fdb70607615e3c5ecc5e79ffe1577c6036
khaoularj <khaoula.raja1@gmail.com>
2024-05-14 09:04:33 -0700

    Parametrize templates

--- 0x02-i18n/config.py
+++ 0x02-i18n/config.py
@@ -6,5 +6,5 @@
 class Config(object):
     """class configuration for babel"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
